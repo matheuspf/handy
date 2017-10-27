1aa830b8edb263b6c8aa3329d9042f17cab00903
matheuspf <matheuspff@gmail.com>
2017-10-27 09:30:44 -0300

    Add chaining of algorithms.

--- include/STLContainer/STLContainer.cpp
+++ include/STLContainer/STLContainer.cpp
@@ -9,24 +9,31 @@ namespace std
     template <typename T>
     std::ostream& operator << (std::ostream& out, const std::vector<T>& v)
     {
-        std::for_each(std::begin(v), std::end(v) - 1, [&](const auto& x)
+        std::for_each(std::begin(v), std::begin(v) + v.size() - 1, [&](const auto& x)
         {
             out << x << " ";    
         });
 
-        out << v.back() << "\n";
+        return out << v.back() << "\n";
     }
 }
 
 
 
+using namespace algc;
 
+
+void lel () {}
 
 int main ()
 {
     std::vector<int> v{1, 4, 3, 7, 0, 2, 5, 6, 9, 8};
 
-    std::cout << algc::all_of(v, [](int x){ return x > -1; }) << "\n";
+    v = (v & sort_(std::greater<int>{})) & sort_();
+
+    //v = sort(v);
+
+    std::cout << v << "\n";
 
 
     return 0;
