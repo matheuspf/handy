0f710b4294db96a2de6d9d7ea8bbf011828ba2c1
matheuspf <matheuspedrozaferreira@uft.edu.br>
2018-06-04 18:27:34 -0300

    Fix travis build

--- tests/external/gtest.cmake
+++ tests/external/gtest.cmake
@@ -6,6 +6,6 @@ target_link_libraries(tests PUBLIC Threads::Threads)
 execute_process(COMMAND git submodule update --init -- ${PROJECT_SOURCE_DIR}/tests/googletest
                         WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR})
 
-add_subdirectory(${PROJECT_SOURCE_DIR}/tests/googletest)
+add_subdirectory(${PROJECT_SOURCE_DIR}/tests/external/googletest)
 
 target_link_libraries(tests PRIVATE gtest gtest_main gmock)
