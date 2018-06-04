6210f3967ff08a053a17859af1f7acc53bb4fcfc
matheuspf <matheuspedrozaferreira@uft.edu.br>
2018-06-04 17:15:21 -0300

    Add gtest as a git submodule

--- tests/cmake/GTest.cmake
+++ tests/cmake/GTest.cmake
@@ -3,20 +3,9 @@ find_package(Threads REQUIRED)
 target_link_libraries(tests PUBLIC Threads::Threads)
 
 
-include(ExternalProject)
+execute_process(COMMAND git submodule update --init -- ${PROJECT_SOURCE_DIR}/tests/googletest
+                        WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR})
 
-ExternalProject_Add(googletest
-    GIT_REPOSITORY https://github.com/google/googletest.git
-    PREFIX "${CMAKE_CURRENT_BINARY_DIR}"
-    INSTALL_COMMAND ""
-)
+add_subdirectory(${PROJECT_SOURCE_DIR}/tests/googletest)
 
-ExternalProject_Get_Property(googletest source_dir)
-ExternalProject_Get_Property(googletest binary_dir)
-
-
-add_dependencies(tests googletest)
-
-target_include_directories(tests PUBLIC ${source_dir}/googletest/include ${binary_dir}/googlemock/gtest)
-
-target_link_libraries(tests PUBLIC ${binary_dir}/googlemock/gtest/libgtest.a ${binary_dir}/googlemock/gtest/libgtest_main.a)
+target_link_libraries(tests PRIVATE gtest gtest_main gmock)
