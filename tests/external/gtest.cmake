4778920b8137bd10920503ceed6f7fa944e5a731
matheuspf <matheuspedrozaferreira@uft.edu.br>
2020-09-11 06:24:40 -0400

    Update CMakeLists; Update Travis; Update tests

--- tests/external/gtest.cmake
+++ tests/external/gtest.cmake
@@ -1,10 +1,10 @@
 find_package(Threads REQUIRED)
 
-target_link_libraries(tests PUBLIC Threads::Threads)
+target_link_libraries(handy_tests PUBLIC Threads::Threads)
 
 execute_process(COMMAND git submodule update --init -- ${PROJECT_SOURCE_DIR}/tests/external/googletest
                         WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR})
 
 add_subdirectory(${PROJECT_SOURCE_DIR}/tests/external/googletest)
 
-target_link_libraries(tests PRIVATE gtest gtest_main gmock)
+target_link_libraries(handy_tests PRIVATE gtest gtest_main gmock)
